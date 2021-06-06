0f4a234ff8e5438b9118f5e50a3f31acf95fadf3
cswhite2000 <18whitechristop@gmail.com>
2021-06-05 19:30:52 -0700

    Add a title to the chloropleth

--- d3-vis/visualization.js
+++ d3-vis/visualization.js
@@ -1,25 +1,33 @@
 
 //Width and height of map
 var width = 860;
-var height = 400;
+var height = 500;
 
 var lowColor = '#d9d9d9'
 var highColor = '#464abc'
 
 // D3 Projection
 var projection = d3.geoAlbersUsa()
-  .translate([width / 2, height / 2]) // translate to center of screen
+  .translate([width / 2, height / 2 + 10]) // translate to center of screen
   .scale([800]); // scale things down so see entire US
 
 // Define path generator
 var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
   .projection(projection); // tell path generator to use albersUsa projection
 
+
+
 //Create SVG element and append map to the SVG
 var svg = d3.select("body")
   .append("svg")
   .attr("width", width)
   .attr("height", height);
+
+svg.append("text")
+	.attr("x", 150)
+	.attr("y", 40)
+	.attr("font-size", 25)
+	.text("Average Cases Per Capita At Colleges")
 
 // Load in my states data!
 d3.csv("../college_state_averages.csv", function(data) {
